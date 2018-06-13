85d5dcea5c65bd0f3afa7c74b68486c29d8bb353
EC2 Default User <ec2-user@ip-172-31-2-117.us-west-2.compute.internal>
2018-06-13 20:41:10 +0000

    Removing old constants used for referencing data in Redis (no longer needed)

--- src/LogisticSparseTaskS3.cpp
+++ src/LogisticSparseTaskS3.cpp
@@ -69,7 +69,6 @@ bool LogisticSparseTaskS3::get_dataset_minibatch(
 
 void LogisticSparseTaskS3::run(const Configuration& config, int worker) {
   std::cout << "Starting LogisticSparseTaskS3"
-    << " MODEL_GRAD_SIZE: " << MODEL_GRAD_SIZE
     << std::endl;
   uint64_t num_s3_batches = config.get_limit_samples() / config.get_s3_size();
   this->config = config;

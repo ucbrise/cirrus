dc36fcb0d1fd91952818f4445eaf54e39a1b3fe3
Joao <joao@berkeley.edu>
2018-06-27 07:39:09 +0000

    Fixed spacing errors

--- src/Tasks.h
+++ src/Tasks.h
@@ -200,23 +200,29 @@ class LoadingSparseTaskS3 : public MLTask {
 };
 
 class LoadingNetflixTask : public MLTask {
-   public:
-    LoadingNetflixTask(
-        uint64_t model_size,
-        uint64_t batch_size, uint64_t samples_per_batch,
-        uint64_t features_per_sample, uint64_t nworkers,
-        uint64_t worker_id, const std::string& ps_ip,
-        uint64_t ps_port) :
-      MLTask(model_size,
-          batch_size, samples_per_batch, features_per_sample,
-          nworkers, worker_id, ps_ip, ps_port)
-  {}
-    void run(const Configuration& config);
-    SparseDataset read_dataset(const Configuration& config, int&, int&);
-    void check_loading(const Configuration&,
-                       std::unique_ptr<S3Client>& s3_client);
+ public:
+  LoadingNetflixTask(uint64_t model_size,
+                     uint64_t batch_size,
+                     uint64_t samples_per_batch,
+                     uint64_t features_per_sample,
+                     uint64_t nworkers,
+                     uint64_t worker_id,
+                     const std::string& ps_ip,
+                     uint64_t ps_port)
+    : MLTask(model_size,
+             batch_size,
+             samples_per_batch,
+             features_per_sample,
+             nworkers,
+             worker_id,
+             ps_ip,
+             ps_port) {}
+  void run(const Configuration& config);
+  SparseDataset read_dataset(const Configuration& config, int&, int&);
+  void check_loading(const Configuration&,
+                     std::unique_ptr<S3Client>& s3_client);
 
-   private:
+  private:
 };
 
 class PSSparseServerTask : public MLTask {

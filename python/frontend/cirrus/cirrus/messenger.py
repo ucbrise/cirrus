86e18db06ec920d5e6c0dd420f3b8a29e96e77dc
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2018-07-09 21:20:12 -0700

    Added timeout to messenger.py

--- python/frontend/cirrus/cirrus/messenger.py
+++ python/frontend/cirrus/cirrus/messenger.py
@@ -10,13 +10,19 @@ def get_num_lambdas(ip="127.0.0.1", port=1337):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect((ip, port))
     clientsocket.send(GET_NUM_CONNS)  # This sends an 8 to the param server, padded to 32 bytes
-    s = clientsocket.recv(32)
-    return struct.unpack("I", s)[0] - 1   # Subtract 1, as we don't count the clientsocket as a connection
+    clientsocket.settimeout(3)
+    try:
+        s = clientsocket.recv(32)
+        return struct.unpack("I", s)[0] - 1   # Subtract 1, as we don't count the clientsocket as a connection
+    except clientsocket.timeout, e:
+        return -1
 
-
-# FIXME: Add some sort of timeout, in case of error out
 def get_last_time_error(ip="127.0.0.1", port=1338):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     clientsocket.sendto(GET_LAST_TIME_ERROR, (ip, port))
-    s = clientsocket.recv(128)
-    return struct.unpack("dd", s)
+    clientsocket.settimeout(3)
+    try:
+        s = clientsocket.recv(128)
+        return struct.unpack("dd", s)
+    except clientsocket.timeout, e:
+        return -1

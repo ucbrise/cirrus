c29f662214d0151c1c71baefcf5e5aea8e32a9c9
Ryan Yang <yangryan0@gmail.com>
2018-07-11 23:14:41 +0000

    Extended time limit

--- tests/test_travis/test.sh
+++ tests/test_travis/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 50 ./tests/test_travis/worker" "timeout 50 ./tests/test_travis/worker"; do
+for cmd in "timeout 120 ./tests/test_travis/test_ps" "timeout 100 ./tests/test_travis/worker" "timeout 100 ./tests/test_travis/worker"; do
   eval ${cmd} & sleep 5
 done

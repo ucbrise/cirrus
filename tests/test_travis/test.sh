d3d6239a4d421abef450bb8af4facc93b2faf938
Ryan Yang <yangryan0@gmail.com>
2018-07-11 05:24:06 +0000

    added a worker (2 total)

--- tests/test_travis/test.sh
+++ tests/test_travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 50 ./tests/test_travis/worker" "./tests/test_travis/error"; do
+for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 50 ./tests/test_travis/worker" "timeout 50 ./tests/test_travis/worker" "./tests/test_travis/error"; do
   eval ${cmd} &
   sleep 5
 done

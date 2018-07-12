2cf136741626705c9cbb0a3ebff3f970a16971ab
Ryan Yang <yangryan0@gmail.com>
2018-07-12 07:27:49 +0000

    Error checking fixed

--- tests/test_travis/test.sh
+++ tests/test_travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cmd in "timeout 30 ./tests/test_travis/test_ps" "timeout 20 ./tests/test_travis/worker" "timeout 20 ./tests/test_travis/worker"; do
+for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 50 ./tests/test_travis/worker" "timeout 50 ./tests/test_travis/worker"; do
   eval ${cmd} &
   sleep 1
 done

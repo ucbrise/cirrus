e564393c7c11b0c5db4ceeb056e3c92152a06107
Joao Carreira <joao@berkeley.edu>
2018-07-24 18:30:44 -0700

    Update

--- tests/iterator/test_iterator.cpp
+++ tests/iterator/test_iterator.cpp
@@ -2,18 +2,24 @@
 #include <S3IteratorLibsvm.h>
 #include <Configuration.h>
 #include <SparseDataset.h>
+#include <SparseLRModel.h>
 #include <memory>
 
 int main() {
   cirrus::s3_initialize_aws();
-  cirrus::Configuration conf;
-  cirrus::S3IteratorLibsvm iter(conf,
+  cirrus::Configuration config;
+  config.read("criteo_libsvm.cfg");
+  cirrus::S3IteratorLibsvm iter(config,
                                 "criteo-kaggle2014-train-libsvm-100k",
                                 "criteo.kaggle2014.train.svm_100K",
                                 58000000, 20, 0, false);
 
+  cirrus::SparseLRModel model(1 << config.get_model_bits());
   while (1) {
     std::shared_ptr<cirrus::SparseDataset> mb = iter.getNext();
+    auto gradient = model.minibatch_grad_sparse(*mb, config);
+
+    model.sgd_update(0.0001, gradient.get());
 
     mb->print();
   }

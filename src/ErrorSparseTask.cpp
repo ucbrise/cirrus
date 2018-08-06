e3ef5b0d14bd7a8b3fca0054f80badb19cde07be
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2018-08-06 16:25:25 -0700

    Fixed ErrorSparseTask formatting issue

--- src/ErrorSparseTask.cpp
+++ src/ErrorSparseTask.cpp
@@ -59,7 +59,8 @@ ErrorSparseTask::ErrorSparseTask(uint64_t model_size,
 }
 
 std::unique_ptr<CirrusModel> get_model(const Configuration& config,
-        const std::string& ps_ip, uint64_t ps_port) {
+                                       const std::string& ps_ip,
+                                       uint64_t ps_port) {
   static PSSparseServerInterface* psi;
   static bool first_time = true;
   if (first_time) {
@@ -77,7 +78,7 @@ std::unique_ptr<CirrusModel> get_model(const Configuration& config,
   }
 
   bool use_col_filtering =
-    config.get_model_type() == Configuration::COLLABORATIVE_FILTERING;
+      config.get_model_type() == Configuration::COLLABORATIVE_FILTERING;
   return psi->get_full_model(use_col_filtering);
 }
 std::unique_ptr<CirrusModel> get_model(const Configuration& config,
@@ -157,57 +158,56 @@ void ErrorSparseTask::run(const Configuration& config) {
   if (config.get_model_type() == Configuration::LOGISTICREGRESSION) {
     left = config.get_test_range().first;
     right = config.get_test_range().second;
-  } else if (config.get_model_type() == Configuration::COLLABORATIVE_FILTERING) {
+  } else if (config.get_model_type() ==
+             Configuration::COLLABORATIVE_FILTERING) {
     left = config.get_train_range().first;
     right = config.get_train_range().second;
   } else {
     exit(-1);
   }
 
-  S3SparseIterator s3_iter(left, right, config,
-      config.get_s3_size(), config.get_minibatch_size(),
+  S3SparseIterator s3_iter(
+      left, right, config, config.get_s3_size(), config.get_minibatch_size(),
       // use_label true for LR
-      config.get_model_type() == Configuration::LOGISTICREGRESSION,
-      0, false);
+      config.get_model_type() == Configuration::LOGISTICREGRESSION, 0, false);
 
   // get data first
   // what we are going to use as a test set
   std::vector<SparseDataset> minibatches_vec;
   std::cout << "[ERROR_TASK] getting minibatches from "
-    << config.get_train_range().first << " to "
-    << config.get_train_range().second
-    << std::endl;
+            << config.get_train_range().first << " to "
+            << config.get_train_range().second << std::endl;
 
   uint32_t minibatches_per_s3_obj =
-    config.get_s3_size() / config.get_minibatch_size();
+      config.get_s3_size() / config.get_minibatch_size();
   for (uint64_t i = 0; i < (right - left) * minibatches_per_s3_obj; ++i) {
     const void* minibatch_data = s3_iter.get_next_fast();
-    SparseDataset ds(reinterpret_cast<const char*>(minibatch_data),
+    SparseDataset ds(
+        reinterpret_cast<const char*>(minibatch_data),
         config.get_minibatch_size(),
         config.get_model_type() == Configuration::LOGISTICREGRESSION);
     minibatches_vec.push_back(ds);
   }
 
-  std::cout << "[ERROR_TASK] Got "
-    << minibatches_vec.size() << " minibatches"
-    << "\n";
+  std::cout << "[ERROR_TASK] Got " << minibatches_vec.size() << " minibatches"
+            << "\n";
   std::cout << "[ERROR_TASK] Building dataset"
-    << "\n";
+            << "\n";
 
   wait_for_start(ERROR_SPARSE_TASK_RANK, nworkers);
   uint64_t start_time = get_time_us();
 
   std::cout << "[ERROR_TASK] Computing accuracies"
-    << "\n";
+            << "\n";
 
   while (1) {
     usleep(ERROR_INTERVAL_USEC);
 
     try {
-      // first we get the model
+// first we get the model
 #ifdef DEBUG
       std::cout << "[ERROR_TASK] getting the full model"
-        << "\n";
+                << "\n";
 #endif
       std::unique_ptr<CirrusModel> model;
       if (is_sharded) {
@@ -219,9 +219,7 @@ void ErrorSparseTask::run(const Configuration& config) {
       std::cout << "[ERROR_TASK] received the model" << std::endl;
 #endif
 
-      std::cout
-        << "[ERROR_TASK] computing loss."
-        << std::endl;
+      std::cout << "[ERROR_TASK] computing loss." << std::endl;
       FEATURE_TYPE total_loss = 0;
       FEATURE_TYPE total_accuracy = 0;
       uint64_t total_num_samples = 0;
@@ -230,7 +228,7 @@ void ErrorSparseTask::run(const Configuration& config) {
 
       for (auto& ds : minibatches_vec) {
         std::pair<FEATURE_TYPE, FEATURE_TYPE> ret =
-          model->calc_loss(ds, start_index);
+            model->calc_loss(ds, start_index);
         total_loss += ret.first;
         total_accuracy += ret.second;
         total_num_samples += ds.num_samples();
@@ -252,17 +250,17 @@ void ErrorSparseTask::run(const Configuration& config) {
                   << " Accuracy: " << (total_accuracy / minibatches_vec.size())
                   << " time(us): " << get_time_us()
                   << " time from start (sec): " << last_time << std::endl;
-      } else if (config.get_model_type() == Configuration::COLLABORATIVE_FILTERING) {
+      } else if (config.get_model_type() ==
+                 Configuration::COLLABORATIVE_FILTERING) {
         last_error = std::sqrt(total_loss / total_num_features);
         std::cout << "[ERROR_TASK] RMSE (Total): " << last_error
                   << " time(us): " << get_time_us()
                   << " time from start (sec): " << last_time << std::endl;
       }
-    } catch(...) {
+    } catch (...) {
       std::cout << "run_compute_error_task unknown id" << std::endl;
     }
   }
 }
 
-} // namespace cirrus
-
+}  // namespace cirrus

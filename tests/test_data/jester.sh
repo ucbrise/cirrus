d9f5b7e3c1552a63314ebed2e097c0ba1f2b5604
Neel Somani <neeljaysomani@gmail.com>
2018-09-02 09:07:27 +0000

    Download Jester CSVs

--- tests/test_data/jester.sh
+++ tests/test_data/jester.sh
@@ -8,6 +8,8 @@ rm jester-data-1.zip
 
 mv jester-data-1.xls tests/test_data/jester-data-1.xls
 
-pip install xlrd
+sudo apt install -y python3-pip
+
+pip3 install xlrd
 
 python3 tests/test_data/parse-jester.py

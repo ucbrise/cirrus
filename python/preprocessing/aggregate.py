ef811d8c3bb7536b021c05b4cba30416d3bb06e4
Neel Somani <neeljaysomani@gmail.com>
2018-09-18 20:29:17 +0000

    Make lambda invocations parallel

--- python/preprocessing/aggregate.py
+++ python/preprocessing/aggregate.py
@@ -1,16 +1,42 @@
 import json
 import boto3
+from threading import Thread
+
+class LocalBounds(Thread):
+    def __init__(self, bucket, src_object):
+        Thread.__init__(self)
+        self.d = {
+            "src_bucket": bucket,
+            "action": "LOCAL_BOUNDS",
+            "src_object": src_object
+        }
+
+    def run(self):
+        l_client = boto3.client("lambda")
+        l_client.invoke(FunctionName="neel_lambda", InvocationType="RequestResponse", LogType="Tail",
+            Payload=json.dumps(self.d))
+
+class LocalScale(LocalBounds):
+    def __init__(self, bucket, src_object, lower, upper):
+        Thread.__init__(self)
+        self.d = {
+            "src_bucket": bucket,
+            "action": "LOCAL_SCALE",
+            "src_object": src_object,
+            "min_v": lower,
+            "max_v": upper
+        }
 
 def MinMaxScaler(bucket_name, objects, lower, upper):
     l_client = boto3.client("lambda")
-    d = {
-        "src_bucket": bucket_name,
-        "action": "LOCAL_BOUNDS"
-    }
+    b_threads = []
     for i in objects:
-        d["src_object"] = i
-        l_client.invoke(FunctionName="neel_lambda", InvocationType="RequestResponse", LogType="Tail",
-                Payload=json.dumps(d))
+        l = LocalBounds(bucket_name, i)
+        l.start()
+        b_threads.append(l)
+
+    for t in b_threads:
+        t.join()
 
     client = boto3.client("s3")
     f_max = {}
@@ -40,12 +66,13 @@ def MinMaxScaler(bucket_name, objects, lower, upper):
 
     s = json.dumps(final)
     client.put_object(Bucket=bucket_name, Key=bucket_name + "_final_bounds", Body=s)
-    d["min_v"] = lower
-    d["max_v"] = upper
-    d["action"] = "LOCAL_SCALE"
+    g_threads = []
     for i in objects:
-        d["src_object"] = i
-        l_client.invoke(FunctionName="neel_lambda", InvocationType="RequestResponse", LogType="Tail",
-                Payload=json.dumps(d))
+        g = LocalScale(bucket_name, i, lower, upper)
+        g.start()
+        g_threads.append(g)
+
+    for t in g_threads:
+        t.join()
 
 MinMaxScaler("criteo-kaggle-19b", ["1"], 0, 1)

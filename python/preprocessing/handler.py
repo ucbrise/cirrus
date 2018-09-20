361d36cfda94337beb7392b9295930bbfe7a10c3
Neel Somani <neeljaysomani@gmail.com>
2018-09-20 22:21:17 +0000

    Update keys for lambda function

--- python/preprocessing/handler.py
+++ python/preprocessing/handler.py
@@ -98,24 +98,25 @@ def serialize_data(data, labels):
 def handler(event, context):
     # Either calculates the local bounds, or scales data and puts the new data in
     # {src_object}_scaled.
-    print(event["src_bucket"], event["src_object"])
+    print(event["s3_bucket_input"], event["s3_key"])
     client = boto3.client("s3")
     if event["action"] == "LOCAL_BOUNDS":
         print("Getting data from S3...")
-        d = get_data_from_s3(client, event["src_bucket"], event["src_object"])
+        d = get_data_from_s3(client, event["s3_bucket_input"], event["s3_key"])
         print("Getting local data bounds...")
         b = get_data_bounds(d)
         print("Putting bounds in S3...")
-        put_bounds_in_s3(client, b, event["src_bucket"], event["src_object"] + "_bounds")
+        put_bounds_in_s3(client, b, event["s3_bucket_input"], event["s3_key"] + "_bounds")
     elif event["action"] == "LOCAL_SCALE":
+        assert "s3_bucket_output" in event, "Must specify output bucket."
         print("Getting data from S3...")
-        d = get_data_from_s3(client, event["src_bucket"], event["src_object"], True)
+        d = get_data_from_s3(client, event["s3_bucket_input"], event["s3_key"], keep_label=True)
         print("Getting global bounds...")
-        b = get_global_bounds(client, event["src_bucket"], event["src_object"])
+        b = get_global_bounds(client, event["s3_bucket_input"], event["s3_key"])
         print("Scaling data...")
         scaled = scale_data(d[0], b, event["min_v"], event["max_v"])
         print("Serializing...")
         serialized = serialize_data(scaled, d[1])
         print("Putting in S3...")
-        client.put_object(Bucket=event["src_bucket"], Key=event["src_object"] + "_scaled", Body=serialized)
+        client.put_object(Bucket=event["s3_bucket_output"], Key=event["s3_key"], Body=serialized)
     return []

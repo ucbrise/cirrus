81eaf14abc24712870ea6041cb5c14eca2b2e6f0
Neel Somani <neel@berkeley.edu>
2018-09-21 13:56:29 -0700

    Removed unused code

--- python/preprocessing/MinMaxHandler.py
+++ python/preprocessing/MinMaxHandler.py
@@ -43,29 +43,3 @@ def scale_data(data, g, min_v, max_v):
                 s = (v - g["min"][idx]) / (g["max"][idx] - g["min"][idx]) * (max_v - min_v) + min_v
             r[j] = (idx, s)
     return data
-
-def handler(event, context):
-    # Either calculates the local bounds, or scales data and puts the new data in
-    # {src_object}_scaled.
-    print(event["s3_bucket_input"], event["s3_key"])
-    client = boto3.client("s3")
-    if event["action"] == "LOCAL_BOUNDS":
-        print("Getting data from S3...")
-        d = get_data_from_s3(client, event["s3_bucket_input"], event["s3_key"])
-        print("Getting local data bounds...")
-        b = get_data_bounds(d)
-        print("Putting bounds in S3...")
-        put_bounds_in_s3(client, b, event["s3_bucket_input"], event["s3_key"] + "_bounds")
-    elif event["action"] == "LOCAL_SCALE":
-        assert "s3_bucket_output" in event, "Must specify output bucket."
-        print("Getting data from S3...")
-        d = get_data_from_s3(client, event["s3_bucket_input"], event["s3_key"], keep_label=True)
-        print("Getting global bounds...")
-        b = get_global_bounds(client, event["s3_bucket_input"], event["s3_key"])
-        print("Scaling data...")
-        scaled = scale_data(d[0], b, event["min_v"], event["max_v"])
-        print("Serializing...")
-        serialized = serialize_data(scaled, d[1])
-        print("Putting in S3...")
-        client.put_object(Bucket=event["s3_bucket_output"], Key=event["s3_key"], Body=serialized)
-    return []

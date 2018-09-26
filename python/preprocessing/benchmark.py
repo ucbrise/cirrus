c286e95e8994bea490da65b53b607cc9d1ca7cc5
Neel Somani <neel@berkeley.edu>
2018-09-26 08:33:34 +0000

    Preprocessing works for 1000 chunks

--- python/preprocessing/benchmark.py
+++ python/preprocessing/benchmark.py
@@ -2,6 +2,7 @@ import preprocessing
 import time
 
 start = time.time()
-preprocessing.normalize("criteo-kaggle-19b", "bucket-neel", preprocessing.NormalizationType.MIN_MAX, True, 0, 1, [str(i) for i in range(1, 100)])
+# preprocessing.normalize("criteo-kaggle-19b", "bucket-neel", preprocessing.NormalizationType.MIN_MAX, True, 0, 1, [str(i) for i in range(1, 100)])
+preprocessing.normalize("criteo-kaggle-19b", "bucket-neel", preprocessing.NormalizationType.MIN_MAX, True, 0, 1)
 # preprocessing.normalize("criteo-kaggle-19b", "bucket-neel", preprocessing.NormalizationType.NORMAL)
 print("Total time: {0} seconds".format(time.time() - start))

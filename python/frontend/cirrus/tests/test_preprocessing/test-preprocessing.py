a5676276d24ea7522ac42a5563d00f3dc0a632c5
Neel Somani <neeljaysomani@gmail.com>
2018-09-30 03:49:33 +0000

    Set up test definitions

---
+++ python/frontend/cirrus/tests/test_preprocessing/test-preprocessing.py
@@ -0,0 +1,15 @@
+# Preprocessing module for Cirrus
+
+import sklearn.datasets
+
+def load_data(path):
+    X, y = sklearn.datasets.load_svmlight_file(path)
+    return X, y
+
+def test_simple(src_bucket, dest_bucket, min_v, max_v, objects):
+    # Make sure all data is in bounds, and all data is present
+    pass
+
+def test_exact(src_file, dest_bucket, min_v, max_v, objects):
+    # Test that all data is scaled correctly
+    pass

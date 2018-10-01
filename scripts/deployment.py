4eee84938f2e92a5498007a74f328732f20607dc
Shea <sheaconlon@users.noreply.github.com>
2018-09-30 18:39:23 -0700

    automate_deployment: move EC2 management functionality into a context manager

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -7,24 +7,25 @@ import time
 import socket
 import tempfile
 import datetime
+import traceback
 
 import boto3
 from botocore.exceptions import ClientError
 import paramiko
 
 # Specifications for a compilation EC2 instance.
-REGION = "us-west-1"
-DISK_SIZE = 16  # GB
-AMI_ID = "ami-3a674d5a"  # This is amzn-ami-hvm-2017.03.1.20170812-x86_64-gp2,
+COMPL_REGION = "us-west-1"
+COMPL_DISK_SIZE = 16  # GB
+COMPL_AMI_ID = "ami-3a674d5a"  # This is amzn-ami-hvm-2017.03.1.20170812-x86_64-gp2,
                          #  which is recommended by AWS as of Sep 27, 2018 for
                          #  compiling executables for Lambda.
-INSTANCE_TYPE = "t3.xlarge"
-SSH_USERNAME = "ec2-user"
+COMPL_INSTANCE_TYPE = "t3.xlarge"
+COMPL_SSH_USERNAME = "ec2-user"
 
 # A series of commands that will result in ~/cirrus/src containing Cirrus'
 #   compiled executables. Note that each command is issued separately, so
 #   working directory changes will not persist.
-COMMANDS = """
+COMPL_COMMANDS = """
 # Install some necessary packages.
 yes | sudo yum install git glibc-static openssl-static.x86_64 \
     zlib-static.x86_64 libcurl-devel
@@ -52,24 +53,12 @@ cd cirrus; make -j 10
 # The names of Cirrus' executables.
 EXECUTABLES = ("parameter_server", "ps_test", "csv_to_libsvm")
 
-# The number of seconds to wait between connection attempts to a compilation EC2
-#   instance.
-CONNECT_TIMEOUT = 10
-
-# The number of attempts to make to connect to a compilation EC2 instance. The
-#   instance takes some time to start up and CONNECT_TIMEOUT * CONNECT_TRIES
-#   should be greater than this startup time.
-CONNECT_TRIES = 9
-
 
 def compile(debug=False):
     """Compile Cirrus.
 
     Creates an EC2 key pair, security group, and instance. Uses the instance to
         compile Cirrus, then downloads the compiled executables.
-
-    Since this function interacts with AWS, it requires that AWS credentials be
-        recorded in the filesystem in the manner required by boto3.
 
     Args:
         debug (bool): Whether to print the stdout and stderr of the compilation
@@ -78,88 +67,14 @@ def compile(debug=False):
     Returns:
         dict[str, file]: The compiled executables, as local temporary files.
     """
-    error = None
-    key_pair_created = False
-    security_group = None
-    instance = None
+    ctx = EC2InstanceContextManager(COMPL_REGION, COMPL_DISK_SIZE, COMPL_AMI_ID,
+        COMPL_INSTANCE_TYPE, COMPL_SSH_USERNAME, "compile")
 
-    try:
-        now = datetime.datetime.now()
-        resource_name = now.strftime("cirrus_%a_%b_%w_%H_%M_%S_%p")
-
-        ec2 = boto3.resource("ec2", REGION)
-
-        print("compile: Creating an EC2 key pair.")
-        response = ec2.meta.client.create_key_pair(KeyName=resource_name)
-        key_pair_created = True
-        key_file = tempfile.TemporaryFile("w+")
-        key_file.write(response["KeyMaterial"])
-        key_file.seek(0)
-
-        print("compile: Creating an EC2 security group.")
-        security_group = ec2.create_security_group(
-            GroupName=resource_name, Description="bla")
-        # Allow SSH access from anywhere so that we can control the instance.
-        security_group.authorize_ingress(
-            IpProtocol="tcp",
-            FromPort=22,
-            ToPort=22,
-            CidrIp="0.0.0.0/0"
-        )
-
-        # Launch EC2 instance.
-        print("compile: Launching an EC2 instance.")
-        # The EC2 instance will be created with an EBS volume that gets deleted
-        #   automatically when the instance is terminated.
-        instances = ec2.create_instances(
-            BlockDeviceMappings=[
-                {
-                    "DeviceName": "/dev/xvda",
-                    "Ebs": {
-                        "DeleteOnTermination": True,
-                        "VolumeSize": DISK_SIZE,
-                    }
-                },
-            ],
-            KeyName=resource_name,
-            ImageId=AMI_ID,
-            InstanceType=INSTANCE_TYPE,
-            MinCount=1,
-            MaxCount=1,
-            SecurityGroups=[resource_name]
-        )
-        instance = instances[0]
-        print("compile: Waiting for the EC2 instance to come up.")
-        instance.wait_until_running()
-        # Reloads metadata about the instance. In particular, retreives its
-        #   public_ip_address.
-        instance.load()
-
-        print("compile: Compiling Cirrus on the EC2 instance.")
-        key = paramiko.RSAKey.from_private_key(key_file)
-        para = paramiko.SSHClient()
-        para.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        for _ in range(CONNECT_TRIES):
-            try:
-                print("compile: Attempting to connect "
-                      "to the EC2 instance using SSH.")
-                para.connect(
-                    hostname=instance.public_ip_address,
-                    username=SSH_USERNAME,
-                    pkey=key,
-                    timeout=CONNECT_TIMEOUT
-                )
-            except socket.timeout:
-                pass
-            except paramiko.ssh_exception.NoValidConnectionsError:
-                time.sleep(CONNECT_TIMEOUT)
-                pass
-            else:
-                break
+    with ctx as ssh:
         # There is a leading and a trailing blank line in the command list.
-        for command in COMMANDS.split("\n")[1:-1]:
+        for command in COMPL_COMMANDS.split("\n")[1:-1]:
             print("compile: Running a command:", command)
-            _, stdout, stderr = para.exec_command(command)
+            _, stdout, stderr = ssh.exec_command(command)
             # exec_command is asynchronous. This waits for completion.
             stdout.channel.recv_exit_status()
             if debug:
@@ -169,40 +84,169 @@ def compile(debug=False):
                 print(stderr.read())
 
         print("compile: Downloading the executables from the EC2 instance.")
-        sftp = para.open_sftp()
+        sftp = ssh.open_sftp()
         local_executables = {}
         for executable in EXECUTABLES:
             print("compile: Download an executable:", executable)
-            remote_path = f"/home/{SSH_USERNAME}/cirrus/src/{executable}"
+            remote_path = f"/home/{COMPL_SSH_USERNAME}/cirrus/src/{executable}"
             local_executables[executable] = tempfile.TemporaryFile()
             sftp.getfo(remote_path, local_executables[executable])
             local_executables[executable].seek(0)
         sftp.close()
-    except (Exception, KeyboardInterrupt, SystemExit) as e:
-        print("compile: An error occured.")
-        error = e
 
-    try:
-        print("compile: Deleting EC2 resources.")
-        if instance is not None:
-            instance.terminate()
-            print("compile: Waiting for the EC2 instance to terminate.")
-            instance.wait_until_terminated()
-        if security_group is not None:
-            security_group.delete()
-        if key_pair_created:
-            ec2.KeyPair(resource_name).delete()
-    except (Exception, KeyboardInterrupt, SystemExit) as e:
-        print("compile: An error occured while attempting to delete EC2 "
-              "resources. You must delete them yourself. There may be a key "
-              "pair, a security group, and an instance.")
-        if error is None:
-            error = e
+        return local_executables
 
-    if error is not None:
-        raise error
 
-    return local_executables
+class EC2InstanceContextManager:
+    """A context manager that creates an EC2 instance and connects to it over
+        SSH. Since this class interacts with AWS, it requires that AWS
+        credentials be recorded in the filesystem in the manner required by
+        boto3.
+    """
+
+    # The number of seconds to wait between SSH connection attempts.
+    _CONNECT_TIMEOUT = 10
+
+    # The number of SSH connection attempts to make. Instances take some time to
+    #   start up; _CONNECT_TIMEOUT * _CONNECT_TRIES should be greater than this
+    #   startup time.
+    _CONNECT_TRIES = 9
+
+    def __init__(self, region, disk_size, ami_id, instance_type, ssh_username,
+                 log_prefix):
+        """Create an EC2 instance context manager.
+
+        Args:
+            region (str): The region to create the instance in.
+            disk_size (int): The amount of disk space to give the instance, in
+                gigabytes.
+            ami_id (str): The ID of the AMI to create the instance from.
+            instance_type (str): The type of instance to create.
+            ssh_username (str): The username to use when accessing the instance
+                via SSH.
+            log_prefix (str): A string to prefix log messages with when printing
+                them out.
+        """
+        self._region = region
+        self._disk_size = disk_size
+        self._ami_id = ami_id
+        self._instance_type = instance_type
+        self._ssh_username = ssh_username
+        self._log_prefix = log_prefix
+        now = datetime.datetime.now()
+        self._resource_name = now.strftime("cirrus_%a_%b_%w_%H_%M_%S_%p")
+        self._key_pair_created = False
+        self._security_group = None
+        self._instance = None
+
+    def __enter__(self):
+        """Create the instance and connect to it via SSH.
+
+        Returns:
+            paramiko.client.SSHClient: An SSH connection to the instance.
+        """
+        try:
+            self._ec2 = boto3.resource("ec2", self._region)
+
+            print(f"{self._log_prefix}: Creating an EC2 key pair.")
+            response = self._ec2.meta.client.create_key_pair(
+                KeyName=self._resource_name)
+            self._key_pair_created = True
+            key_file = tempfile.TemporaryFile("w+")
+            key_file.write(response["KeyMaterial"])
+            key_file.seek(0)
+
+            print(f"{self._log_prefix}: Creating an EC2 security group.")
+            self._security_group = self._ec2.create_security_group(
+                GroupName=self._resource_name, Description="bla")
+            # Allow SSH access from anywhere so that we can control the
+            #   instance.
+            self._security_group.authorize_ingress(
+                IpProtocol="tcp",
+                FromPort=22,
+                ToPort=22,
+                CidrIp="0.0.0.0/0"
+            )
+
+            # Launch EC2 instance.
+            print(f"{self._log_prefix}: Launching an EC2 instance.")
+            # The EC2 instance will be created with an EBS volume that gets
+            #   deleted automatically when the instance is terminated.
+            instances = self._ec2.create_instances(
+                BlockDeviceMappings=[
+                    {
+                        "DeviceName": "/dev/xvda",
+                        "Ebs": {
+                            "DeleteOnTermination": True,
+                            "VolumeSize": self._disk_size,
+                        }
+                    },
+                ],
+                KeyName=self._resource_name,
+                ImageId=self._ami_id,
+                InstanceType=self._instance_type,
+                MinCount=1,
+                MaxCount=1,
+                SecurityGroups=[self._resource_name]
+            )
+            self._instance = instances[0]
+            print(f"{self._log_prefix}: Waiting for the EC2 instance to come "
+                   "up.")
+            self._instance.wait_until_running()
+            # Reloads metadata about the instance. In particular, retreives its
+            #   public_ip_address.
+            self._instance.load()
+
+            key = paramiko.RSAKey.from_private_key(key_file)
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            success = False
+            for _ in range(self._CONNECT_TRIES):
+                try:
+                    print(f"{self._log_prefix}: Attempting to connect "
+                           "to the EC2 instance using SSH.")
+                    ssh.connect(
+                        hostname=self._instance.public_ip_address,
+                        username=self._ssh_username,
+                        pkey=key,
+                        timeout=self._CONNECT_TIMEOUT
+                    )
+                except socket.timeout:
+                    pass
+                except paramiko.ssh_exception.NoValidConnectionsError:
+                    time.sleep(self._CONNECT_TIMEOUT)
+                    pass
+                else:
+                    success = True
+                    break
+            if success:
+                return ssh
+            else:
+                raise RuntimeError("Failed to connect to the EC2 instance.")
+        except (Exception, KeyboardInterrupt, SystemExit) as e:
+            print(f"{self._log_prefix}: An error occured.")
+            traceback.print_exc(e)
+            self.__exit__()
+            raise e
+
+    def __exit__(self, *args):
+        """Delete the instance and any other AWS resources created for it.
+        """
+        try:
+            if self._instance is not None:
+                self._instance.terminate()
+                print(f"{self._log_prefix}: Waiting for the EC2 instance to "
+                       "terminate.")
+                self._instance.wait_until_terminated()
+            if self._security_group is not None:
+                self._security_group.delete()
+            if self._key_pair_created:
+                self._ec2.KeyPair(self._resource_name).delete()
+        except (Exception, KeyboardInterrupt, SystemExit) as e:
+            print(f"{self._log_prefix}: An error occured while attempting to "
+                  "delete EC2 resources. You must delete them yourself. There "
+                  "may be a key pair, a security group, and an instance.")
+            raise e
 
 
 if __name__ == "__main__":

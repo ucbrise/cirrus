b423ca4b2f4c3e28dae7938445d3cfb8766d3fa6
Joao Carreira <joao@berkeley.edu>
2018-11-01 13:42:01 -0700

    Improve test

--- tests/test_travis_lr/test_register_worker.cpp
+++ tests/test_travis_lr/test_register_worker.cpp
@@ -2,12 +2,14 @@
 
 using namespace cirrus;
 
+#define HIGH_TIMEOUT (1000)
+
 int main() {
   std::unique_ptr<PSSparseServerInterface> psi =
       std::make_unique<PSSparseServerInterface>("127.0.0.1", 1337);
   psi->connect();
 
-  uint32_t registered = psi->register_task(0, 100);
+  uint32_t registered = psi->register_task(0, HIGH_TIMEOUT);
 
   if (registered != 0) {
     throw std::runtime_error("Expected 0");
@@ -15,11 +17,11 @@ int main() {
 
   std::cout << "Deregister task" << std::endl;
   registered = psi->deregister_task(0);
-  if (registered != 1) {
+  if (registered != 0) {
     throw std::runtime_error("Expected 1");
   }
 
-  registered = psi->register_task(0, 100);
+  registered = psi->register_task(0, HIGH_TIMEOUT);
   if (registered != 1) {
     throw std::runtime_error("Expected 1");
   }
@@ -27,6 +29,27 @@ int main() {
   registered = psi->deregister_task(0);
   if (registered != 1) {
     throw std::runtime_error("Expected 1");
+  }
+
+  // test that timeout works
+
+  // register taks with id 1 and timeout 0 seconds
+  registered = psi->register_task(1, 0);
+  if (registered != 0) {
+    throw std::runtime_error("Expected 1");
+  }
+
+  sleep(5);
+  // after 5 seconds task should have been deregistered
+
+  registered = psi->deregister_task(1);
+  if (registered != 1) {
+    throw std::runtime_error("Expected 1");
+  }
+
+  registered = psi->deregister_task(2);
+  if (registered != 2) {
+    throw std::runtime_error("Expected 2");
   }
 
   return 0;

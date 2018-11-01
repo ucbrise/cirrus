b79a8b58c3dafd83aaa8347bcad0133b681de4e2
Joao Carreira <joao@berkeley.edu>
2018-11-01 12:57:58 -0700

    Refactor test

--- tests/test_travis/test.sh
+++ tests/test_travis/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 50 ./tests/test_travis/worker" "timeout 50 ./tests/test_travis/worker"; do
-  eval ${cmd} &
-  sleep 1
-done
+timeout 60 ./tests/test_travis/test_ps&
+sleep 1
+
+timeout 50 ./tests/test_travis/worker&
+sleep 1
+
+timeout 50 ./tests/test_travis/worker&
+sleep 1

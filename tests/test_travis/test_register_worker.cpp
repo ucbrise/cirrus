493bb32704013f07dfa3e7dfc9b8bf0cf7340478
Joao Carreira <joao@berkeley.edu>
2018-11-01 12:53:49 -0700

    Added test for register/deregister task API

---
+++ tests/test_travis/test_register_worker.cpp
@@ -0,0 +1,33 @@
+#include <PSSparseServerInterface.h>
+
+using namespace cirrus;
+
+int main() {
+  std::unique_ptr<PSSparseServerInterface> psi =
+      std::make_unique<PSSparseServerInterface>("127.0.0.1", 1337);
+  psi->connect();
+
+  uint32_t registered = psi->register_task(0, 100);
+
+  if (registered != 0) {
+    throw std::runtime_error("Expected 0");
+  }
+
+  std::cout << "Deregister task" << std::endl;
+  registered = psi->deregister_task(0);
+  if (registered != 1) {
+    throw std::runtime_error("Expected 1");
+  }
+  
+  registered = psi->register_task(0, 100);
+  if (registered != 1) {
+    throw std::runtime_error("Expected 1");
+  }
+  
+  registered = psi->deregister_task(0);
+  if (registered != 1) {
+    throw std::runtime_error("Expected 1");
+  }
+
+  return 0;
+}

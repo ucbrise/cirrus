5385f3f231c565fdecf51e6789ca3b1ec382f487
Shea <sheaconlon@users.noreply.github.com>
2018-11-27 14:31:31 -0800

    automate_deployment: switch all boto usage over to ClientManager

--- python/frontend/cirrus/cirrus/automate.py
+++ python/frontend/cirrus/cirrus/automate.py
@@ -16,6 +16,7 @@ from . import handler
 from . import configuration
 from .instance import Instance
 from . import utilities
+from .clients import clients
 
 # The type of instance to use for compilation.
 BUILD_INSTANCE_TYPE = "c4.4xlarge"
@@ -99,167 +100,6 @@ MIN_GENERATION_TIME = 10
 # The minimum number of concurrent executions that AWS requires an account to
 #   keep unreserved. Current as of 11/21/18.
 _MINIMUM_UNRESERVED_CONCURRENCY = 100
-
-
-class ClientManager(object):
-    """A manager of cached AWS clients.
-    """
-
-    def __init__(self):
-        """Create a client manager.
-
-        Clients will not yet be initialized.
-        """
-        # These mutexes synchronize reading/writing of the respective client
-        #   attributes.
-        self._lamb_mutex = threading.Lock()
-        self._lamb_low_retries_mutex = threading.Lock()
-        self._iam_mutex = threading.Lock()
-        self._ec2_mutex = threading.Lock()
-        self._cloudwatch_logs_mutex = threading.Lock()
-        self._s3_mutex = threading.Lock()
-
-        self.clear_cache()
-        self._log = logging.getLogger("cirrus.automate.ClientManager")
-
-
-    @property
-    def lamb(self):
-        """Get a Lambda client.
-
-        Initializes one if none is cached.
-
-
-        Returns:
-            botocore.client.BaseClient: The client.
-        """
-        with self._lamb_mutex:
-            if self._lamb is None:
-                self._log.debug("ClientManager: Initializing Lambda client.")
-                self._lamb = boto3.client(
-                    "lambda",
-                    configuration.config()["aws"]["region"]
-                )
-        return self._lamb
-
-    @property
-    def lamb_low_retries(self):
-        """Get a Lambda client that only retries requests once.
-
-        Initializes one if none is cached.
-
-        Returns:
-            botocore.client.BaseClient: The client.
-        """
-        with self._lamb_low_retries_mutex:
-            if self._lamb_low_retries is None:
-                self._log.debug("ClientManager: Initializing no-retries Lambda "
-                                "client.")
-                region = configuration.config()["aws"]["region"]
-                config = botocore.config.Config(retries={"max_attempts": 1})
-                self._lamb_low_retries = boto3.client("lambda", region,
-                                                      config=config)
-        return self._lamb_low_retries
-
-
-    @property
-    def iam(self):
-        """Get an IAM resource.
-
-        Initializes one if none is cached.
-
-        Returns:
-            boto3.resources.base.ServiceResource: The resource.
-        """
-        with self._iam_mutex:
-            if self._iam is None:
-                self._log.debug("ClientManager: Initializing IAM resource.")
-                self._iam = boto3.resource(
-                    "iam",
-                    configuration.config()["aws"]["region"]
-                )
-        return self._iam
-
-
-    @property
-    def ec2(self):
-        """Get an EC2 client.
-
-        Initializes one if none is cached.
-
-        Returns:
-            botocore.client.BaseClient: The client.
-        """
-        with self._ec2_mutex:
-            if self._ec2 is None:
-                self._log.debug("ClientManager: Initializing EC2 client.")
-                self._ec2 = boto3.client(
-                    "ec2",
-                    configuration.config()["aws"]["region"]
-                )
-        return self._ec2
-
-
-    @property
-    def cloudwatch_logs(self):
-        """Get a Cloudwatch Logs client.
-
-        Initializes one if none is cached.
-
-        Returns:
-            botocore.client.BaseClient: The client.
-        """
-        with self._cloudwatch_logs_mutex:
-            if self._cloudwatch_logs is None:
-                self._log.debug("ClientManager: Initializing Cloudwatch Logs "
-                                "client.")
-                self._cloudwatch_logs = boto3.client(
-                    "logs",
-                    configuration.config()["aws"]["region"]
-                )
-        return self._cloudwatch_logs
-
-
-    @property
-    def s3(self):
-        """Get an S3 resource.
-
-        Initializes one if none is cached.
-
-        Returns:
-            boto3.resources.base.ServiceResource: The resource."""
-        with self._s3_mutex:
-            if self._s3 is None:
-                self._log.debug("ClientManager: Initializing S3 resource.")
-                self._s3 = boto3.resource(
-                    "s3", configuration.config()["aws"]["region"])
-        return self._s3
-
-
-    def clear_cache(self):
-        """Clear any cached clients.
-        """
-        with self._lamb_mutex:
-            self._lamb = None
-
-        with self._lamb_low_retries_mutex:
-            self._lamb_low_retries = None
-
-        with self._iam_mutex:
-            self._iam = None
-
-        with self._ec2_mutex:
-            self._ec2 = None
-
-        with self._cloudwatch_logs_mutex:
-            self._cloudwatch_logs = None
-
-        with self._s3_mutex:
-            self._s3 = None
-
-
-# Cached AWS clients to be used throughout this module.
-clients = ClientManager()
 
 
 class ParameterServer(object):
@@ -703,13 +543,12 @@ def make_lambda_package(path, executables_path):
         zip.writestr(info, handler_source)
 
         log.debug("make_lambda_package: Initializing S3.")
-        s3_client = boto3.client("s3")
         executable = io.BytesIO()
 
         log.debug("make_lambda_package: Downloading executable.")
         executables_path += "/amazon/parameter_server"
         bucket, key = _split_s3_url(executables_path)
-        s3_client.download_fileobj(bucket, key, executable)
+        clients.s3.meta.client.download_fileobj(bucket, key, executable)
 
         log.debug("make_lambda_package: Writing executable.")
         info = zipfile.ZipInfo("parameter_server")
@@ -720,8 +559,8 @@ def make_lambda_package(path, executables_path):
     log.debug("make_lambda_package: Uploading package.")
     file.seek(0)
     bucket, key = _split_s3_url(path)
-    s3_client.upload_fileobj(file, bucket, key,
-        ExtraArgs={"ACL": "public-read"})
+    clients.s3.meta.client.upload_fileobj(file, bucket, key,
+                                          ExtraArgs={"ACL": "public-read"})
 
     log.debug("make_lambda_package: Waiting for changes to take effect.")
     # Waits for S3's eventual consistency to catch up. Ideally, something more
@@ -795,7 +634,7 @@ def get_bucket_name():
     log = logging.getLogger("cirrus.automate.get_bucket_name")
 
     log.debug("get_bucket_name: Retreiving account ID.")
-    account_id = boto3.client("sts").get_caller_identity().get("Account")
+    account_id = clients.sts.get_caller_identity().get("Account")
 
     return BUCKET_BASE_NAME + "-" + account_id
 

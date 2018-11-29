6059df04c114b69738201758c170cb6c991eb04e
Shea <sheaconlon@users.noreply.github.com>
2018-11-29 13:34:59 -0800

    automate_deployment: make bug fix to make_server_image

--- python/frontend/cirrus/cirrus/automate.py
+++ python/frontend/cirrus/cirrus/automate.py
@@ -607,7 +607,7 @@ def make_server_image(name, executables_path):
     # Why twice? Sometimes it didn't know about the awscli package unless I
     #   updated twice. It might just be due to timing.
     instance.run_command("sudo apt update")
-    instance.run_command("sudo apt update")
+    instance.run_command("sudo apt update", False)
     instance.run_command("yes | sudo apt install awscli")
 
     # Install some useful tools.

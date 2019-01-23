d05ae11218608d0e1b7e492be2f81af6ade694d4
Shea <sheaconlon@users.noreply.github.com>
2019-01-23 15:56:36 -0800

    make user verify before deleting functions

--- python/scripts/delete_lambdas.py
+++ python/scripts/delete_lambdas.py
@@ -7,5 +7,6 @@ REGION = "us-west-2"
 client = boto3.client("lambda", REGION)
 response = client.list_functions()
 for function_info in response["Functions"]:
-    client.delete_function(FunctionName=function_info["FunctionName"])
-    print("Deleted %s." % function_info["FunctionName"])
+    resp = raw_input("Delete %s?\ny/n: " % function_info["FunctionName"])
+    if resp == "y":
+        client.delete_function(FunctionName=function_info["FunctionName"])
